2252fbbfdde5fa829240b48940f910dee7c1d53c
YiwenShang <yiwenshangbjut@163.com>
2023-03-07 00:14:44 +0000

    init commit

---
+++ build/precache-manifest.116f13678f3270ae3efd3c262d7edb9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48a0775296cb8d1bdf35",
+    "url": "/static/css/main.35f69faf.chunk.css"
+  },
+  {
+    "revision": "48a0775296cb8d1bdf35",
+    "url": "/static/js/main.2b311f7a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "20a05c1a170c401b1991",
+    "url": "/static/js/2.48dee5f6.chunk.js"
+  },
+  {
+    "revision": "9e5efe10aac8a9644dbabc5e14dcf993",
+    "url": "/index.html"
+  }
+];
